f6ddf6cef9cbbdafb87d842e6d2c08b1b2852333
vl <vl@nextadnet.com>
2015-03-17 16:33:43 +0200

    Added more dispatcher tests

--- dispatcher_test.go
+++ dispatcher_test.go
@@ -2,8 +2,189 @@ package gorpc
 
 import (
 	"fmt"
+	"io"
 	"testing"
+	"unsafe"
 )
+
+func TestDispatcherEmptyFuncName(t *testing.T) {
+	d := NewDispatcher()
+	testPanic(t, func() {
+		d.RegisterFunc("", func() {})
+	})
+}
+
+func TestDispatcherInvalidFirstArgType(t *testing.T) {
+	d := NewDispatcher()
+	testPanic(t, func() {
+		d.RegisterFunc("foo", func(clientAddr bool, request string) {})
+	})
+}
+
+func TestDispatcherInvalidSecondResType(t *testing.T) {
+	d := NewDispatcher()
+	testPanic(t, func() {
+		d.RegisterFunc("foo", func() (response int, err float64) { return })
+	})
+}
+
+func TestDispatcherTooManyArgs(t *testing.T) {
+	d := NewDispatcher()
+	testPanic(t, func() {
+		d.RegisterFunc("foo", func(clientAddr string, foo, bar int) {})
+	})
+}
+
+func TestDispatcherTooManyRes(t *testing.T) {
+	d := NewDispatcher()
+	testPanic(t, func() {
+		d.RegisterFunc("foo", func() (response int, err error, foobar string) { return })
+	})
+}
+
+func TestDispatcherFuncArg(t *testing.T) {
+	d := NewDispatcher()
+	testPanic(t, func() {
+		d.RegisterFunc("foo", func(req func()) {})
+	})
+}
+
+func TestDispatcherChanArg(t *testing.T) {
+	d := NewDispatcher()
+	testPanic(t, func() {
+		d.RegisterFunc("foo", func(req chan int) {})
+	})
+}
+
+func TestDispatcherInterfaceArg(t *testing.T) {
+	d := NewDispatcher()
+	testPanic(t, func() {
+		d.RegisterFunc("foo", func(req io.Reader) {})
+	})
+	testPanic(t, func() {
+		d.RegisterFunc("foo", func(req interface{}) {})
+	})
+}
+
+func TestDispatcherUnsafePointerArg(t *testing.T) {
+	d := NewDispatcher()
+	testPanic(t, func() {
+		d.RegisterFunc("foo", func(req unsafe.Pointer) {})
+	})
+}
+
+func TestDispatcherFuncRes(t *testing.T) {
+	d := NewDispatcher()
+	testPanic(t, func() {
+		d.RegisterFunc("foo", func() (res func()) { return })
+	})
+}
+
+func TestDispatcherChanRes(t *testing.T) {
+	d := NewDispatcher()
+	testPanic(t, func() {
+		d.RegisterFunc("foo", func() (res chan int) { return })
+	})
+}
+
+func TestDispatcherInterfaceRes(t *testing.T) {
+	d := NewDispatcher()
+	testPanic(t, func() {
+		d.RegisterFunc("foo", func() (res io.Reader) { return })
+	})
+	testPanic(t, func() {
+		d.RegisterFunc("foo", func() (res interface{}) { return })
+	})
+}
+
+func TestDispatcherUnsafePointerRes(t *testing.T) {
+	d := NewDispatcher()
+	testPanic(t, func() {
+		d.RegisterFunc("foo", func() (res unsafe.Pointer) { return })
+	})
+}
+
+func TestDispatcherPassStructArgByValue(t *testing.T) {
+	type RequestType struct {
+		a int
+		B string
+	}
+
+	d := NewDispatcher()
+	testPanic(t, func() {
+		d.RegisterFunc("foo", func(request RequestType) {})
+	})
+}
+
+func TestDispatcherReturnStructResByValue(t *testing.T) {
+	type ResponseType struct {
+		A int
+		b string
+	}
+
+	d := NewDispatcher()
+	testPanic(t, func() {
+		d.RegisterFunc("foo", func() (response ResponseType) { return })
+	})
+}
+
+func TestDispatcherPassStructArgNoExportedFields(t *testing.T) {
+	type RequestTypeNoExport struct {
+		a int
+		b string
+	}
+
+	d := NewDispatcher()
+	testPanic(t, func() {
+		d.RegisterFunc("foo", func(req *RequestTypeNoExport) {})
+	})
+}
+
+func TestDispatcherReturnStructResNoExportedFields(t *testing.T) {
+	type ResponseTypeNoExport struct {
+		a int
+		b string
+	}
+
+	d := NewDispatcher()
+	testPanic(t, func() {
+		d.RegisterFunc("foo", func() (resp *ResponseTypeNoExport) { return })
+	})
+}
+
+func TestDispatcherStructsWithIdenticalFields(t *testing.T) {
+	type Struct1 struct {
+		A int
+	}
+	type Struct2 struct {
+		A int
+	}
+
+	d := NewDispatcher()
+
+	d.RegisterFunc("foo", func(request *Struct1) *Struct2 {
+		return &Struct2{
+			A: request.A,
+		}
+	})
+
+	testDispatcher(t, d, func(fc *DispatcherClient) {
+		reqs := &Struct1{
+			A: 12356,
+		}
+		res, err := fc.Call("foo", reqs)
+		if err != nil {
+			t.Fatalf("Unepxected error: [%s]", err)
+		}
+		ress, ok := res.(*Struct2)
+		if !ok {
+			t.Fatalf("Unexpected response type: %T. Expected *Struct2", res)
+		}
+		if ress.A != reqs.A {
+			t.Fatalf("Unexpected response: [%+v]. Expected [%+v]", ress, reqs)
+		}
+	})
+}
 
 func TestDispatcherInvalidArgType(t *testing.T) {
 	d := NewDispatcher()
@@ -99,6 +280,36 @@ func TestDispatcherStructArgCall(t *testing.T) {
 		}
 		if ress.C != reqs.B || ress.D != reqs.A {
 			t.Fatalf("Unexpected response: [%+v]. Expected &ResponseArg{C:%s, D:%d}", ress, reqs.B, reqs.A)
+		}
+	})
+}
+
+func TestDispatcherRecursiveStructArg(t *testing.T) {
+	type RecMsg struct {
+		A   int
+		Rec *RecMsg
+	}
+
+	d := NewDispatcher()
+	d.RegisterFunc("foo", func(req *RecMsg) *RecMsg { return req })
+
+	testDispatcher(t, d, func(fc *DispatcherClient) {
+		reqs := &RecMsg{
+			A: 1,
+			Rec: &RecMsg{
+				A: 2,
+			},
+		}
+		res, err := fc.Call("foo", reqs)
+		if err != nil {
+			t.Fatalf("Unexpected error: [%s]", err)
+		}
+		ress, ok := res.(*RecMsg)
+		if !ok {
+			t.Fatalf("Unepxected response type: %T. Expected *RecMsg", ress)
+		}
+		if ress.A != reqs.A || ress.Rec.A != reqs.Rec.A {
+			t.Fatalf("Unepxected respons: [%+v]. Expected [%+v]", ress, reqs)
 		}
 	})
 }
@@ -241,4 +452,14 @@ func testDispatcher(t *testing.T, d *Dispatcher, f func(fc *DispatcherClient)) {
 	fc := d.NewFuncClient(c)
 
 	f(fc)
+}
+
+func testPanic(t *testing.T, f func()) {
+	defer func() {
+		if r := recover(); r != nil {
+			return
+		}
+		t.Fatalf("Panic expected")
+	}()
+	f()
 }
